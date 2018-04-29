a450de59d4bd9c90fae9aeb68234640231984746
cashlionjp <npaterno@gmail.com>
2018-04-28 20:16:25 -0400

    Update to ES2015 class's

--- Agent.js
+++ Agent.js
@@ -1,22 +1,24 @@
-function Agent() {
-    //                                            ____
-    // Users are expected to prototype at least a live function 
-    // 
-    // As well as any others you might need!!
-    this.type = null;
-    this.score = 1;
-    this.weighted = 1;
-    this.config = {};
-    this.dna = [];
-    this.copy = function () {
-        let agent = new Agent();
-        for (var prop in this.config) {
-            if (this.config.hasOwnProperty(prop)) {
-                agent.config[prop] = this.config[prop];
+class Agent {
+    constructor() {
+        //                                          ____     ______
+        // Users are expected to prototype at least live and mutate functions
+        // 
+        // As well as any others you might need!!
+        this.type = null;
+        this.score = 1;
+        this.weighted = 1;
+        this.config = {};
+        this.dna = [];
+        this.copy = function () {
+            let agent = new Agent();
+            for (var prop in this.config) {
+                if (this.config.hasOwnProperty(prop)) {
+                    agent.config[prop] = this.config[prop];
+                }
             }
-        }
-        agent.type = this.type;
-        agent.dna = this.dna.slice();
-        return agent;
+            agent.type = this.type;
+            agent.dna = this.dna.slice();
+            return agent;
+        };
     }
 }
