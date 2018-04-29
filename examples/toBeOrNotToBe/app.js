93c40d67cd86c160a7fcc2761ec1013af2c70b71
cashlionjp <npaterno@gmail.com>
2018-04-28 20:27:01 -0400

    Update: Add comments to example

--- examples/toBeOrNotToBe/app.js
+++ examples/toBeOrNotToBe/app.js
@@ -1,27 +1,34 @@
 Agent.prototype.live = function () {  // Required: Live function
-    // let count = 0;
+    //   Linear increase per correct letter
     for (let i = 0; i < this.dna.length; i++) {
         if (this.dna[i] === evolve[i]) {
             this.score++;
-            // this.score += ++count * count;
         }
     }
+
+    // Try above snippet and below separately
+    // Compare results with a 
+    
+    //    Factorial increase per correct letter
+    // let count = 0;
+    // for (let i = 0; i < this.dna.length; i++) {
+    //     if (this.dna[i] === evolve[i]) {
+    //         this.score += ++count * count;
+    //     }
+    // }
 };
 
 Agent.prototype.mutate = function (rate) {  // Required: Mutate function
     for (let i = 0; i < this.dna.length; i++) {
         if (Math.random() < rate) {
-            let letter = randLetter();
-            if (letter === undefined) debugger;
-            this.dna[i] = letter;
+            this.dna[i] = randLetter(); // Randomly mutate a letter
         }
     }
     if (Math.random() < rate) {
-        let letter = randLetter();
-        this.dna.push(letter);
+        this.dna.push(randLetter()); // Randomly increase sentence by a letter
     }
     if (Math.random() < rate) {
-        this.dna.splice(this.dna.length - 1, 1)
+        this.dna.splice(this.dna.length - 1, 1) // Randomly decrease sentence by a letter
     }
 };
 
@@ -35,25 +42,23 @@ let word = "";
 let best = null;
 
 let results = document.getElementById("results");
-let killSwitch = 0; // Kill switch to limit iterations.
 let genCount = 0;  // Count the number of generations.
-while (evolve !== word && killSwitch < 1000) {
+while (evolve !== word && genCount < 1000) { // Limit generations to 1000
     ga.run().generation();
     best = ga.culture.best;
     word = best.dna.join('');
     var p = document.createElement("p");
     results.prepend(`Generation: ${++genCount}   Best: ${word}`, p);
-    killSwitch++;
 }
 
 
 function createSentence(index) {
-    // Create a sentence with a random number of words.
     let sentence = new Agent();
-    // Create a word with a random number of letters.
+    // Create a sentence with a random number of words.
     let numOfWords = Math.floor(1 + Math.random() * MAX_NUM_OF_WORDS);
-    let numOfLetters = Math.floor(1 + Math.random() * MAX_WORD_LENGTH);
     for (let i = 0; i < numOfWords; i++) {
+        // Create a word with a random number of letters.
+        let numOfLetters = Math.floor(1 + Math.random() * MAX_WORD_LENGTH);
         for (let j = 0; j < numOfLetters; j++) {
             let letter = randLetter();
             sentence.dna.push(letter);
