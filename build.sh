f15a489432d5605ed0d216db9b674ab64e5a23c7
cashlionjp <npaterno@gmail.com>
2018-05-02 00:34:46 -0400

    Update github pages layout and docs

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 mkdir -p "$(pwd)/dist/"
 touch "$(pwd)/dist/JeneticS.js"
-cat "$(pwd)/src/JeneticS.js" > "$(pwd)/dist/JeneticS.js"
+cat "$(pwd)/src/JeneticS.js" > "$(pwd)/docs/dist/JeneticS.js"
 echo "\n\n" >> "$(pwd)/dist/JeneticS.js"
-cat "$(pwd)/src/Culture.js" >> "$(pwd)/dist/JeneticS.js"
+cat "$(pwd)/src/Culture.js" >> "$(pwd)/docs/dist/JeneticS.js"
 echo "\n\n" >> "$(pwd)/dist/JeneticS.js"
-cat "$(pwd)/src/Agent.js" >> "$(pwd)/dist/JeneticS.js"
-uglifyjs "$(pwd)/dist/JeneticS.js" -o "$(pwd)/dist/JeneticS.min.js" -m -c
+cat "$(pwd)/src/Agent.js" >> "$(pwd)/docs/dist/JeneticS.js"
+uglifyjs "$(pwd)/docs/dist/JeneticS.js" -o "$(pwd)/docs/dist/JeneticS.min.js" -m -c
